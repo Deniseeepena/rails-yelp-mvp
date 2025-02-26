d088a05d25313a35e3ad2b04c0fec5aec0756316
Denise Pena <Denisepena312@gmail.com>
2025-02-26 15:37:18 +0100

    yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+  Rails.application.routes.draw do
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :restaurants, only: %i[index new create show] do
+      resources :reviews, only: %i[index create]
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,44 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Wiping seeds'
+Restaurant.destroy_all
+
+puts 'Creating seeds'
+
+Restaurant.create(
+  name: 'Paulitos Sashimi',
+  address: '123 Meow Street',
+  phone_number: '01234 578990',
+  category: 'chinese'
+)
+
+Restaurant.create(
+  name: 'Timos bird food',
+  address: '456 Pasta Lane',
+  phone_number: '01234 840200',
+  category: 'italian'
+)
+
+Restaurant.create(
+  name: 'Ozges ikram',
+  address: '789 Blossom Ave',
+  phone_number: '01234 283648',
+  category: 'japanese'
+)
+
+Restaurant.create(
+  name: 'Artems burger',
+  address: '101 Baguette Blvd',
+  phone_number: '01234 627394',
+  category: 'french'
+)
+
+Restaurant.create(
+  name: 'Valentijns Bitterballen',
+  address: '202 Syrup St',
+  phone_number: '01234 987123',
+  category: 'belgian'
+)
+
+puts 'Seeds created!'
